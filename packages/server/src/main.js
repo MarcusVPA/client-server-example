d2b226f3f0b5e21bb9d7e9b367a7aaf3b4dc5536
Marcus Vinicius <marcus_mayme@hotmail.com>
2022-05-26 23:41:57 -0300

    Apollo Server

--- packages/server/src/main.js
+++ packages/server/src/main.js
@@ -1,9 +1,48 @@
 import express from "express";
-import cors from "cors";
+import { ApolloServer, gql } from "apollo-server-express";
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
 
-const server = express();
+const app = express();
 
-server.get("/status", (_,response) => {
+async function startServer() { 
+  const server = new ApolloServer({
+    plugins: [
+      ApolloServerPluginLandingPageGraphQLPlayground(),
+    ],
+    typeDefs: gql `
+      type Client {
+        id: ID!
+        name: String!
+      }
+
+      type Demand {
+        id: ID!
+        name: String!
+        client: Client!
+        deadline: String
+      }
+
+      type Query {
+        demands: [Demand]!
+      }
+    `,
+    resolvers: {
+      Query: {
+        demands: () => [],
+      },
+    }
+  });
+  await server.start();
+  server.applyMiddleware({
+    app,
+    cors: {
+      origin: "http://localhost:3000",
+    },
+  });
+};
+startServer(app);
+
+/*server.get("/status", (_,response) => {
   response.send({
     status: "Okay",
   });
@@ -18,11 +57,11 @@ server
       response.send({
         Okay: true,
       });
-  });
+  });*/
 
 const PORT = process.env.PORT ? parseInt(process.env.PORT): 8000;
 const HOSTNAME = process.env.HOSTNAME || "127.0.0.1" ;
 
-server.listen(PORT, HOSTNAME, () => {
+app.listen(PORT, HOSTNAME, () => {
   console.log(`Server is listening at http://${HOSTNAME}:${PORT}`);
 });
